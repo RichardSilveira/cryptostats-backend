ea21264437bc63308840b894f0122654e35d3589
Richard Lee da Silva Silveira <rlee@hagerty.com>
2022-05-09 15:51:42 -0400

    chore: explore the app setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { CoinbaseModule } from './coinbase/coinbase.module';
+// import { AuthModule } from './auth/auth.module';
+// import { CoinbaseModule } from './coinbase/coinbase.module';
 import { UsersModule } from './users/users.module';
-
+//todo: Enable UsersModule
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGODB_URI'),
-      }),
-      inject: [ConfigService],
-    }),
-    UsersModule,
-    AuthModule,
-    CoinbaseModule,
+      UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,24 +1,35 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
-import { CurrentUser } from '../auth/current-user.decorator';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+// import { CurrentUser } from '../auth/current-user.decorator';
+// import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CreateUserRequest } from './dto/request/create-user-request.dto';
 import { UserResponse } from './dto/response/user-response.dto';
-import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
 
   @Post()
   async createUser(
     @Body() createUserRequest: CreateUserRequest,
   ): Promise<UserResponse> {
-    return this.usersService.createUser(createUserRequest);
+    return {
+      _id: 'b11bd76f-e436-4689-82c6-5b3dba6a5b78',
+      email: 'user@gmail.com',
+      isCoinbaseAuthorized: true,
+    };
   }
 
   @Get()
-  @UseGuards(JwtAuthGuard)
-  async getUser(@CurrentUser() user: UserResponse): Promise<UserResponse> {
-    return user;
+  async getUser(user: UserResponse): Promise<UserResponse> {
+    return {
+      _id: 'b11bd76f-e436-4689-82c6-5b3dba6a5b78',
+      email: 'user@gmail.com',
+      isCoinbaseAuthorized: true,
+    };
   }
+
+  // @Get()
+  // @UseGuards(JwtAuthGuard)
+  // async getUser(@CurrentUser() user: UserResponse): Promise<UserResponse> {
+  //   return user;
+  // }
 }
